c8e7309ff4128e646ad2f7bd09841a6a5641e293
CrossNox <ijmermet@gmail.com>
2020-10-22 19:04:01 -0300

    add initial user model

--- users_microservice/models.py
+++ users_microservice/models.py
@@ -1,4 +1,12 @@
 """SQLAlchemy models."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils.types.email import EmailType
 
 db = SQLAlchemy()
+
+
+class User(db.Model):  # type:ignore
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String)
+    apellido = db.Column(db.String)
+    email = db.Column(EmailType)
