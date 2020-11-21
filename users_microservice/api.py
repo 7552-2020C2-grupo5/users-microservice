f44b2c837e8b9cbda41058828ac525e1c2d84ba9
Leonardo <lbellaera@fi.uba.ar>
2020-11-21 04:47:03 -0300

    Agrega cambios para soportar manejo de logins

--- users_microservice/api.py
+++ users_microservice/api.py
@@ -1,11 +1,15 @@
 """API module."""
 from flask_restx import Api, Resource, fields
-from users_microservice.models import db, User
+from flask import request, Response
+from users_microservice.models import db, User, bcrypt
 from users_microservice import __version__
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+MIMETYPE = "application/json"
 
 
 api = Api(
@@ -26,34 +30,68 @@ def handle_exception(error: Exception):
     return {'message': message}, getattr(error, 'code', 500)
 
 
-user_model = api.model(
-    "User",
+register_model = api.model(
+    "User register model",
     {
         "id": fields.Integer(readonly=True, description="The user unique identifier"),
-        "first_name": fields.String(required=True, description='The name'),
-        "last_name": fields.String(required=True, description='The last name'),
-        "email": fields.String(required=True, description='The email'),
+        "first_name": fields.String(required=True, description='The user first name'),
+        "last_name": fields.String(required=True, description='The user last name'),
+        "email": fields.String(required=True, description='The user email'),
+        "password": fields.String(required=True, description='The user password'),
     },
 )
+
+login_model = api.model(
+    "User login model",
+    {
+        "email": fields.String(required=True, description='The user email'),
+        "password": fields.String(required=True, description='The user password'),
+    },
+)
+
+profile_model = api.model(
+    "User profile model",
+    {
+        "id": fields.Integer(readonly=True, description="The user unique identifier"),
+        "first_name": fields.String(required=True, description='The user first name'),
+        "last_name": fields.String(required=True, description='The user last name'),
+        "email": fields.String(required=True, description='The user email'),
+    },
+)
+
+
+@api.route('/validate_token')
+class UserTokenValidatorResource(Resource):
+    """
+    User Token Validator
+    """
+
+    @api.doc('validate_user_token')
+    def get(self):
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[1]
+        else:
+            auth_token = ''
+
+        fail_msg = {'status': 'fail'}
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if isinstance(resp, int):
+                msg = {'status': 'success'}
+                return Response(json.dumps(msg), status=200, mimetype=MIMETYPE)
+            return Response(json.dumps(fail_msg), status=401, mimetype=MIMETYPE)
+        else:
+            return Response(json.dumps(fail_msg), status=401, mimetype=MIMETYPE)
 
 
 @api.route('/user')
 class UserListResource(Resource):
     @api.doc('list_user')
-    @api.marshal_list_with(user_model)
+    @api.marshal_list_with(profile_model)
     def get(self):
         """Get all users."""
         return User.query.all()
-
-    @api.doc('create_user')
-    @api.expect(user_model)
-    @api.marshal_with(user_model)
-    def post(self):
-        """Create a new user."""
-        new_user = User(**api.payload)
-        db.session.add(new_user)
-        db.session.commit()
-        return new_user
 
 
 @api.route('/user/<int:user_id>')
@@ -61,8 +99,76 @@ class UserListResource(Resource):
 @api.response(404, 'User not found')
 class UserResource(Resource):
     @api.doc('get_user')
-    @api.marshal_with(user_model)
+    @api.marshal_with(profile_model)
     def get(self, user_id):
         """Get a user by id."""
         user = User.query.filter(User.id == user_id).first()
+        if not user:
+            return None, 404
         return user
+
+
+@api.route('/register')
+class RegisterResource(Resource):
+    """
+    User Registration Resource
+    """
+
+    @api.expect(register_model, validate=True)
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(email=data.get('email')).first()
+        if not user:
+            user = User(**api.payload)
+            db.session.add(user)
+            db.session.commit()
+
+            auth_token = user.encode_auth_token(user.id)
+            resp = {
+                'status': 'success',
+                'message': 'Successfully registered.',
+                'auth_token': auth_token.decode(),
+            }
+            msg = json.dumps(resp)
+            return Response(msg, status=201, mimetype='application/json')
+        else:
+            resp = {
+                'status': 'fail',
+                'message': 'User already exists. Please Log in.',
+            }
+            msg = json.dumps(resp)
+            return Response(msg, status=202, mimetype='application/json')
+
+
+@api.route('/login')
+class LoginResource(Resource):
+    """
+    User Login Resource
+    """
+
+    @api.expect(login_model, validate=True)
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(email=data.get('email')).first()
+
+        if user and bcrypt.check_password_hash(user.password, data.get('password')):
+            auth_token = user.encode_auth_token(user.id)
+            resp = {
+                'status': 'success',
+                'message': 'Successfully logged in.',
+                'auth_token': auth_token.decode(),
+            }
+            msg = json.dumps(resp)
+            return Response(msg, status=200, mimetype='application/json')
+
+        elif user and not bcrypt.check_password_hash(
+            user.password, data.get('password')
+        ):
+            resp = {'status': 'fail', 'message': 'Password does not match.'}
+            msg = json.dumps(resp)
+            return Response(msg, status=202, mimetype='application/json')
+
+        else:
+            resp = {'status': 'fail', 'message': 'User does not exist.'}
+            msg = json.dumps(resp)
+            return Response(msg, status=202, mimetype='application/json')
--- users_microservice/models.py
+++ users_microservice/models.py
@@ -1,12 +1,52 @@
 """SQLAlchemy models."""
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils.types.email import EmailType
+from users_microservice.cfg import SECRET_KEY
+from flask_bcrypt import Bcrypt
+import datetime
+import jwt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 class User(db.Model):  # type:ignore
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    email = db.Column(EmailType)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(EmailType, unique=True, nullable=False)
+
+    def __init__(self, first_name, last_name, email, password):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = bcrypt.generate_password_hash(password).decode()
+
+    @staticmethod
+    def encode_auth_token(user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id,
+        }
+        return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, SECRET_KEY)
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return "Signature expired. Please log in again."
+        except jwt.InvalidTokenError:
+            return "Invalid token. Please log in again."
