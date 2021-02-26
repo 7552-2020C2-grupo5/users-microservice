05471969fa624661f5a74e21963323320cdbad37
CrossNox <ijmermet@gmail.com>
2021-02-26 04:37:41 -0300

    RFC: Identidad federada (#45)
    * 2big2fail2lazy2describe
    
    * 2big2fail2lazy2describe
    
    * bump minor

--- users_microservice/api.py
+++ users_microservice/api.py
@@ -4,6 +4,7 @@ from flask_restx import Api
 
 from users_microservice import __version__
 from users_microservice.namespaces.admins import api as admins_namespace
+from users_microservice.namespaces.oauth import api as oauth_namespace
 from users_microservice.namespaces.users import api as users_namespace
 
 api = Api(
@@ -17,6 +18,7 @@ api = Api(
 )
 api.add_namespace(users_namespace, path='/users')
 api.add_namespace(admins_namespace, path='/admins')
+api.add_namespace(oauth_namespace, path='/oauth')
 
 
 @api.errorhandler
